279497b19fb10591b3d50f2bc432ea405bfc6064
William Johnston <wjoh0002@student.monash.edu>
2020-04-30 13:44:19 +1000

    intern-page basic layout and calendar functionality working

--- src/templates/intern-page.js
+++ src/templates/intern-page.js
@@ -9,7 +9,24 @@ import SEO from "~components/SEO";
 import { fancyLog } from "~utils/helpers";
 
 class InternPageComponent extends Component {
-  state = {};
+  numDummyDays = 31;
+
+  dummyDayData = Array(this.numDummyDays)
+    .fill(null)
+    .map((_, i) => {
+      return {
+        dayCount: i + 1,
+        date: `${i + 1}.7`,
+        learned: `I learned this thing`
+      };
+    });
+
+  constructor(props) {
+    super(props);
+    this.state = { openCalendarSquare: null };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
 
   componentDidMount() {
     fancyLog(`Intern page`);
@@ -20,7 +37,26 @@ class InternPageComponent extends Component {
   //
   // those affect the state variables
 
-  // handleClick = () => {}
+  handleClick = e => {
+    const day = parseInt(
+      e.currentTarget.attributes.getNamedItem(`data-day-count`).value
+    );
+    this.setState({
+      openCalendarSquare: day
+    });
+  };
+
+  handleMouseLeave = e => {
+    const day = parseInt(
+      e.currentTarget.attributes.getNamedItem(`data-day-count`).value
+    );
+    // If we just mouse-left the current open square, then close it
+    if (this.state.openCalendarSquare === day) {
+      this.setState({
+        openCalendarSquare: null
+      });
+    }
+  };
   // handleHover = () => {}
 
   //
@@ -29,7 +65,6 @@ class InternPageComponent extends Component {
     const { frontmatter, location } = this.props;
 
     // @Will you'll be spending most of your time in here
-
     return (
       <>
         <SEO
@@ -39,10 +74,57 @@ class InternPageComponent extends Component {
           path={location.pathname}
         />
 
-        <Layout className="intern-page w-full relative flex flex-col justify-between pt-12">
-          {/* banner section */}
+        <Layout className="intern-page w-full px-40 relative flex flex-col justify-between pt-12">
+          <section className="banner w-full h-screen flex flex-col justify-center text-center">
+            <p>IN TERN PRESENTS</p>
+            <h1 className="text-huge font-bold">One Thing I Learned Today</h1>
+            <p>FOR THREE MONTHS. AT LOVE + MONEY.</p>
+            <p>200 GERTRUDE STREET, FITZROY, INTERNET.</p>
+            <p>&#x2193;</p>
+          </section>
 
-          {/* calendar section */}
+          <section className="calendar w-full flex flex-col">
+            <div className="flex flex-row text-center">
+              <div className="w-20vw">M</div>
+              <div className="w-20vw">T</div>
+              <div className="w-20vw">W</div>
+              <div className="w-20vw">T</div>
+              <div className="w-20vw">F</div>
+            </div>
+            <div className="flex flex-row flex-wrap">
+              {this.dummyDayData.map(({ dayCount, date, learned }, i) => {
+                return (
+                  // @Dan how do we make these square? flex-wrap and width = vh doesnt work
+                  //      at the momment these are width=20% and height = some fixed amount
+                  //
+                  // @Dan how do we make the borders blend in with each other? Atm we just have regular dotted borders
+                  //
+                  // @Dan this class list logic looks messy af, I sometimes use a front-end package called classnames to clean up classname logic, lemme know if its okay to install that.
+                  <div
+                    className={`calendar-square relative w-1/5 h-32 border-dotted border border-white ${this
+                      .state.openCalendarSquare === dayCount &&
+                      `calendar-square--clicked`} ${this.state
+                      .openCalendarSquare !== dayCount && `cursor-pointer`}`}
+                    data-day-count={dayCount}
+                    onClick={this.handleClick}
+                    onMouseLeave={this.handleMouseLeave}
+                    // @Dan I'm new to the following 3 things but ESLint said I had to - assuming those are your settings?
+                    role="textbox"
+                    tabIndex={i}
+                    onKeyDown={this.handleClick}
+                  >
+                    <div className="calendar-square__underlay w-full h-full absolute p-4 flex flex-col justify-between">
+                      <div className="self-end">{date}</div>
+                      <div className="text-2xl">{`Day ${dayCount}`}</div>
+                    </div>
+                    <div className="calendar-square__overlay transition-opacity w-full h-full absolute p-4 bg-white text-black">
+                      {learned}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </section>
           {/* - fixed M-F elements at the top */}
           {/* - grid with hover effects and click effects */}
 
